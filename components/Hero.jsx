1a98f2638c2221c536b10f6b6f64c864742c6b1e
Charles Karlo Leguarda <charlesleguarda@gmail.com>
2023-03-14 15:04:50 +0800

    1st commit

---
+++ components/Hero.jsx
@@ -0,0 +1,30 @@
+import Image from 'next/image'
+import React from 'react'
+
+import deceImg from '../public/assets/decentralized2.jpg'
+
+const Hero = () => {
+  return (
+    <div className='w-full h-[90vh] top-[90px]'>
+      <Image 
+      className='h-full w-full absolute -z-10' 
+      src={deceImg} 
+      alt='/' 
+      objectFit='cover'/>
+      <div className='w-full h-[90%] flex flex-col justify-center items-center text-center text-white px-4'>
+        <h1>Decentralized</h1>
+        <h1 className='py-2'><span className='blue'>Trading</span> Protocol</h1>
+        <p className='text-xl py-4'>Guaranteed liquidity trading for millions of users and top Etherium applications.</p>
+      <div className=''>
+        <button className='m-2'>Use Defi</button>
+        <button className='m-2'>FAQ</button>
+      </div>
+      </div>
+      <div>
+        <p className='text-center text-white text-2xl font-bold'>Total Volume Secured: $42,104,783,662.47</p>
+        </div>
+    </div>
+  )
+}
+
+export default Hero
